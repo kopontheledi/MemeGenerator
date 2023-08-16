b918e3f5706367ad8a17b6b5135183ab783a9210
Kopo Ntheledi <kopontheledi@gmail.com>
2023-08-16 16:05:29 +0200

    updates

--- Meme-Generator/src/App.jsx
+++ Meme-Generator/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme"
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import Footer from "./components/Footer";
+import "./App.css";
 
 export default function App() {
-    return (
-        <div>
-            <Header />
-            <Meme />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Meme />
+      <Footer />
+    </div>
+  );
 }
---
+++ Meme-Generator/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+       <h4 className="footer">Created @2023 by Kopo Ntheledi, All rights reserved.</h4> 
+    )
+}
